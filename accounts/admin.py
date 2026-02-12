e673d6ed5534db7d436235327ab281e67032f6f1
mourya-rr-rakesh <mouryarakesh512@gmail.com>
2026-02-12 13:29:10 +0530

    Delete accounts directory

--- accounts/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import User
-
-@admin.register(User)
-class UserAdmin(admin.ModelAdmin):
-    list_display = ('username', 'email', 'first_name', 'role', 'org', 'phone', 'subscription_status')
-    list_filter = ('role', 'subscription_status')
-    search_fields = ('username', 'email', 'first_name', 'org')
-    ordering = ('username',)
--- accounts/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import register, login_view, logout_view, profile
-
-urlpatterns = [
-    path('register.html', register, name='api_register'),
-    path('login.html', login_view, name='api_login'),
-    path('logout.html', logout_view, name='api_logout'),
-    path('profile.html', profile, name='api_profile'),
-]
